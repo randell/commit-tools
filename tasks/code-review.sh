b293f31a6b716806868e983ad51c0af68a771767
Steve Worley <steven.worley@acquia.com>
2016-09-13 10:38:11 +1000

    Adds diff-filter to select files
    Updates the pre-commit to determine changed files with --diff-filter. This will allow us to exclude deleted files from the diff list so we don't need additional processing of the file list in commit tasks.

--- tasks/code-review.sh
+++ tasks/code-review.sh
@@ -7,33 +7,22 @@
 
 ROOT=`pwd`
 PHPCS="$ROOT/vendor/bin/phpcs"
-UPSTREAM=$1
-FILES=$2
-LINTED_FILES=''
 
-for f in $FILES
+CHANGED_FILES=$2
+
+for FILE in $CHANGED_FILES
 do
-
-  if [ ! -f $f ]; then
-    # If the file has been deleted we can skip this file.
-    continue
-  fi
-
   # Lint the files to ensure no syntax errors.
-  php -l $f
+  php -l $FILE
   if [ $? != 0 ]; then
 		echo "Fix the error before commit."
 		exit 1
   fi
-
-  # Keep track of the files that have been linted as these will be passed to
-  # phpcs for code standard checking.
-  LINTED_FILES="$LINTED_FILES $f"
 done
 
 # Run phpcs across changed files.
 # @TODO: Could potentially include the Drupal best practice sniff from coder.
-$PHPCS --standard=Drupal -n -p $LINTED_FILES
+$PHPCS --standard=Drupal -n -p $CHANGED_FILES
 if [ $? != 0 ]; then
 	echo "Fix the error before commit."
 	exit 1
