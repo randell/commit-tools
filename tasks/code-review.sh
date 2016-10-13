9f69df48e2879e5d5cc845cfc96550c7b55d8bd5
Roman Barbun <roman.barbun@acquia.com>
2016-10-13 17:01:49 +1100

    Added an ability to bypass contrib modules through security and code review.

--- tasks/code-review.sh
+++ tasks/code-review.sh
@@ -7,11 +7,20 @@
 
 ROOT=`pwd`
 PHPCS="$ROOT/vendor/bin/phpcs"
+EXCLUDE="/modules/contrib"
 
-CHANGED_FILES=$2
+# Shift the passed arguments to get the list of changed files.
+shift
+CHANGED_FILES=$@
+declare -a SNIFF_FILES
 
 for FILE in $CHANGED_FILES
 do
+  # Exclude the files that we dont want to sniff.
+  if [[ ! $FILE =~ $EXCLUDE ]]; then
+    SNIFF_FILES=("${SNIFF_FILES[@]}" "$FILE")
+  fi
+
   # Lint the files to ensure no syntax errors.
   php -l $FILE
   if [ $? != 0 ]; then
@@ -22,8 +31,10 @@ done
 
 # Run phpcs across changed files.
 # @TODO: Could potentially include the Drupal best practice sniff from coder.
-$PHPCS --standard=Drupal -n -p $CHANGED_FILES
-if [ $? != 0 ]; then
-	echo "Fix the error before commit."
-	exit 1
+if [ ! -z "$SNIFF_FILES" ]; then
+  $PHPCS --standard=Drupal -n -p ${SNIFF_FILES[@]}
+  if [ $? != 0 ]; then
+	  echo "Fix the error before commit."
+	  exit 1
+  fi
 fi
